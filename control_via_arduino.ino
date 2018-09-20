122c230c992b8b1dd7dad872c8a22a6b0098a379
sarachayamnual <33593250+sarachayamnual@users.noreply.github.com>
2018-09-20 15:59:12 +0700

    new fixing Arduino code

---
+++ control_via_arduino.ino
@@ -0,0 +1,33 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+int readstate = 0;
+
+void clotheslineOff() {  
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(9, LOW);
+    delay(1000);
+   
+}
+void clotheslineOn() {
+    digitalWrite(9, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+    delay(1000);
+}
+
+void loop() {
+  readstate = Serial.read(), "0" ;
+  if (readstate == 48) {
+    // 48 = 0
+    clotheslineOff();
+  }
+  else if (readstate == 49){
+    // 49 = 1
+    clotheslineOn();
+  } 
+} 
